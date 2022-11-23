4482c18f675c25dae8c382401b7a14d90c754ba4
hms-challenger <hendrik_ms@gmx.de>
2022-11-23 19:18:49 +0100

    fixed ftp upload timeout=30, seperate py-file for maintenance

---
+++ ftp_upload.py
@@ -0,0 +1,64 @@
+import os
+import shutil
+import ftplib
+# import getpass
+from ftplib import FTP_TLS
+from config import FTP_HOST, FTP_USER, FTP_PASS
+
+# save connection to ftp-server
+try:
+    print("Connecting to minimusiker ftp server!") 
+    ftp = FTP_TLS(FTP_HOST, timeout=30)
+    # passwd = getpass("Enter your password: ")
+    ftp.login(FTP_USER, FTP_PASS)
+    ftp.prot_p()  
+    ftp.encoding = "utf-8"
+    ftp.cwd("htdocs/hoerthin/mp3")
+    print("Connection success! Directory: ", ftp.pwd())
+except ftplib.all_errors as e:
+    print('FTP error:', e)
+
+def uploadThis(uploadFolder):
+    files = os.listdir(uploadFolder)
+    os.chdir(uploadFolder)
+    for f in files:
+        print("Uploading...", f)
+        if os.path.isfile(uploadFolder + r'/{}'.format(f)):
+            fh = open(f, 'rb')
+            ftp.storbinary('STOR %s' % f, fh)
+            fh.close()
+        elif os.path.isdir(uploadFolder + r'/{}'.format(f)):
+            ftp.mkd(f)
+            ftp.cwd(f)
+            uploadThis(uploadFolder + r'/{}'.format(f))
+    ftp.cwd('..')
+    os.chdir('..')
+
+desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
+
+for folderName in os.listdir(desktop):
+    if folderName.startswith("MM"):
+        splitFolderName = folderName.split()
+        schoolID = splitFolderName[0][2:]
+        schoolName = splitFolderName[1]
+    
+base = desktop + "/MM" + schoolID + " " + schoolName
+mp3Folder = base + "/mp3"
+uploadFolder = base + "/" + str(schoolID)
+os.rename(os.path.join(base, mp3Folder), os.path.join(base, schoolID).replace('mp3', schoolID))
+
+# upload mp3Folder to ftp server
+print("Creating mp3 folder on ftp server!")
+ftp.mkd(schoolID)
+ftp.cwd(schoolID)
+# uploadThis(uploadFolder)
+
+ftp.quit()
+
+# delete cache Folder 
+try:
+    shutil.rmtree(base)
+    print("cache folder deleted!")
+except OSError as e:
+    print("Error: %s - %s." % (e.filename, e.strerror))
+print("Job done!")
